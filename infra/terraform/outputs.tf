482a07e22f9701b659e93eca410b784e061115ce
saurabhchako89 <saurabhchako89@gmail.com>
2025-11-21 16:35:02 -0500

    Removed duplicate variables from main.tf (region, compartment_id, ssh_public_key)
    Fixed outputs.tf to reference correct instance name hermes_instance instead of generated_oci_core_instance
    
    Added missing db_password variable to GitHub Actions workflow

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,19 +1,14 @@
 output "instance_id" {
   description = "OCID of the created instance"
-  value       = oci_core_instance.generated_oci_core_instance.id
-}
-
-output "public_ip" {
-  description = "Public IP address of the instance"
-  value       = oci_core_instance.generated_oci_core_instance.public_ip
+  value       = oci_core_instance.hermes_instance.id
 }
 
 output "private_ip" {
   description = "Private IP address of the instance"
-  value       = oci_core_instance.generated_oci_core_instance.private_ip
+  value       = oci_core_instance.hermes_instance.private_ip
 }
 
 output "instance_state" {
   description = "Current state of the instance"
-  value       = oci_core_instance.generated_oci_core_instance.state
+  value       = oci_core_instance.hermes_instance.state
 }
