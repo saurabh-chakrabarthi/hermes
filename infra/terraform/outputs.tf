5511c218b504bc00e0475ce605fe50108ac7c65f
saurabhchako89 <saurabhchako89@gmail.com>
2025-11-23 16:28:46 -0500

    update terraform

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,14 +1,14 @@
 output "instance_id" {
-  description = "OCID of the created instance"
-  value       = oci_core_instance.hermes_instance.id
+  description = "OCID of the instance"
+  value = local.should_create_instance ? oci_core_instance.hermes_instance[0].id : data.oci_core_instances.existing_instances.instances[0].id
 }
 
 output "private_ip" {
   description = "Private IP address of the instance"
-  value       = oci_core_instance.hermes_instance.private_ip
+  value = local.should_create_instance ? oci_core_instance.hermes_instance[0].private_ip : data.oci_core_instances.existing_instances.instances[0].private_ip
 }
 
 output "instance_state" {
   description = "Current state of the instance"
-  value       = oci_core_instance.hermes_instance.state
+  value = local.should_create_instance ? oci_core_instance.hermes_instance[0].state : data.oci_core_instances.existing_instances.instances[0].state
 }
