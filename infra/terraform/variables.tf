8df5a2d8e9c0efd7eafed8353d9e4be1b4df805d
saurabhchako89 <saurabhchako89@gmail.com>
2026-01-04 17:25:12 -0500

    Force clean deployment: increment trigger to 8
    - Clean up: removed redundant files and mongodb.properties
    - Fixed: docker-compose to use GHCR images
    - Fixed: setup-docker.sh to use environment variables only
    - Trigger: force new instance creation for clean deployment

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -44,7 +44,7 @@ variable "tenancy_ocid" {
 variable "deployment_trigger" {
   description = "Trigger to force instance replacement"
   type        = string
-  default     = "7"
+  default     = "8"
 }
 
 variable "github_owner" {
